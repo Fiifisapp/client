880fddd99570c2fbe01cae38f53eaebbbf0c747a
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2022-05-25 02:44:34 +0000

    logic completed

--- src/components/AddDocument/AddDocument.js
+++ src/components/AddDocument/AddDocument.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
-const AddDocument = () => {
+const AddDocument = ({ open, onClose }) => {
+  const navigate = useNavigate();
+  if (!open) return null;
+
   return (
     <div>
-        <div className="overlay">
-            <div className="modal-container">
-                <h2>X</h2>
-            </div>
+      <div className="overlay" onClick={onClose}>
+        <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+          <h2 onClick={onClose}>X</h2>
+          <p>click to upload a PDF or text</p>
+          <div className="button-container">
+            <button>upload PDF</button>
+            <button>upload text</button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddDocument
+export default AddDocument;
