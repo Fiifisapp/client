cb80b0af4f1f54f6bfd6df6ad3f447ce61a881ed
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2022-05-25 02:34:02 +0000

    layed the structure

---
+++ src/components/AddDocument/AddDocument.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddDocument = () => {
+  return (
+    <div>
+        <div className="overlay">
+            <div className="modal-container">
+                <h2>X</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddDocument
